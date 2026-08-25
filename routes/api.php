cdb0362232dcb51fe5c32be7dab4e61195683162
Victor <vmmzdev@gmail.com>
2026-08-25 02:52:06 +0200

    feat:rutas de la web dentro del middleware auth para que no carge datos si el usuario no esta autentificado

--- routes/api.php
+++ routes/api.php
@@ -13,35 +13,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/user', [UserController::class, 'index']);
+Route::middleware('auth')->group(function () {
+Route::apiResource('users', UserController::class);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::apiResource('purchases', PurchaseController::class);
 Route::apiResource('products', ProductController::class);
-  Route::apiResource('sellers', SellerController::class);
-
+Route::apiResource('sellers', SellerController::class);
 Route::apiResource('sales', SaleController::class);
 Route::get('/dashboard', [DashboardController::class, 'index']);
-
-Route::middleware('auth')->group(function () {
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    // Route::get('/user', [AuthController::class, 'user']);
-
-    Route::post('/users/register', [UserController::class, 'register']);
-
-
-
-    // Route::apiResource('products', ProductController::class);
-
-    // Route::apiResource('purchases', PurchaseController::class);
-
-    // Route::apiResource('sales', SaleController::class);
-
-    //Route::apiResource('sellers', SellerController::class);
+Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-
-    Route::apiResource('users', UserController::class);
-});
